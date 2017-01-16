5c6d32aa8e2b64a44200b6b446288bd18a2b7e7e
Kostia Kolesnyk <tiko@utiko.net>
2017-01-16 09:48:07 +0200

    Initial Commit

---
+++ TryCI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryCI
+//
+//  Created by Kostia Kolesnyk on 1/16/17.
+//  Copyright © 2017 Kostia Kolesnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TryCI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TryCI
+//
+//  Created by Kostia Kolesnyk on 1/16/17.
+//  Copyright © 2017 Kostia Kolesnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
